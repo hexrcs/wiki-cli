a038dfcd2e191e632df2ba3f9d5c593fd5cdc8b7
Xiaoru Li <dev@xiaoru.li>
2019-07-21 19:12:48 +0200

    Add app entrance

---
+++ src/index.tsx
@@ -0,0 +1,33 @@
+import parseArgs from "minimist";
+import React from "react";
+import { render } from "ink";
+import { Help } from "./components/Help";
+import { App } from "./components/App";
+import { DEFAULT_LANGUAGE } from "./consts";
+
+main();
+
+async function main() {
+  const argv: ParsedArgs = parseArgs(process.argv.slice(2), {
+    boolean: ["help"],
+    string: ["lang"],
+    alias: { h: "help", l: "lang" },
+  });
+  const showHelp = argv.h;
+
+  if (showHelp) {
+    render(<Help />);
+  } else {
+    const hl = argv.lang || DEFAULT_LANGUAGE;
+    const keywords = argv._.join(" ");
+    render(<App keywords={keywords} hl={hl} />, { exitOnCtrlC: false });
+  }
+}
+
+interface ParsedArgs {
+  _: (string | number)[];
+  help?: boolean;
+  h?: boolean;
+  lang?: string;
+  l?: string;
+}
