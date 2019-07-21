3f347431b2b51f3a3151e5b46ac234a64971f3ce
Xiaoru Li <dev@xiaoru.li>
2019-07-21 18:54:40 +0200

    Init

---
+++ README.md
@@ -0,0 +1,36 @@
+# Wiki CLI?
+
+Search Wikipedia and read summaries directly in your terminal!
+
+## Usage
+
+```
+# The first step is obvious...
+npm i -g wiki-cli
+
+# Then, just call `wiki` alone to enter the interactive mode...
+wiki
+
+# ... or, directly with keywords you'd like to search for...
+wiki Jesus nut
+
+# You can read the not so helpful help page with option `-h` or `--help`...
+wiki -h
+
+# ... or, specify the locale with `-l` or `--lang`
+wiki -l de Rindfleischetikettierungsüberwachungsaufgabenübertragungsgesetz
+```
+
+## Why
+
+Cuz Ink is awesome, React is fun, and JavaScript is everywhere. Also it's a demo app for my CLI talk. And, who doesn't love Wikipedia?
+
+## Todo
+
+See TODO tags in the source code.
+
+Also need to write some more docs.
+
+## License
+
+GPL-3.0
---
+++ src/store.ts
@@ -0,0 +1,85 @@
+import { observable, computed, action } from "mobx";
+import { SearchError, fetchSearchResults, Page } from "./fetch";
+import { DEFAULT_LANGUAGE } from "./consts";
+import { createContext } from "react";
+import debounce from "lodash/debounce";
+
+export class Store {
+  // input
+  @observable
+  input: string = "";
+  @action
+  updateInputs = (keywords: string) => {
+    this.input = keywords;
+    this.debouncedSearch();
+  };
+  @action
+  debouncedSearch = debounce(async () => {
+    if (!this.input.length) {
+      this.results = [];
+      return;
+    }
+    const data = await fetchSearchResults(this.input);
+    if (data.type === "Results") {
+      this.error = null;
+      this.results = data.results;
+    } else {
+      this.error = data;
+      this.results = [];
+    }
+    this.selectedIndex = 0;
+  }, 200);
+
+  // selectedIndex
+  @observable
+  selectedIndex: number = 0;
+  @action
+  selectNext = () => {
+    const index = this.selectedIndex + 1;
+    if (index >= 0 && this.results.length > index) {
+      this.selectedIndex = index;
+    }
+  };
+  @action
+  selectPrev = () => {
+    const index = this.selectedIndex - 1;
+    if (index >= 0 && this.results.length > index) {
+      this.selectedIndex = index;
+    }
+  };
+
+  // results
+  @observable
+  results: Page[] = [];
+  @computed
+  get selectedURL(): string {
+    if (this.selectedIndex >= 0 && this.results.length > this.selectedIndex) {
+      const { pageid } = this.results[this.selectedIndex];
+      return `https://${this.lang}.wikipedia.org/?curid=${pageid}`;
+    } else {
+      return "";
+    }
+  }
+
+  // isDetailsOpen
+  @observable
+  isDetailsOpen: boolean = false;
+  @action
+  toggleDetails = () => {
+    if (this.results.length > 0) {
+      this.isDetailsOpen = !this.isDetailsOpen;
+    }
+  };
+
+  @observable
+  isCleanUpTime: boolean = false;
+
+  @observable
+  lang = DEFAULT_LANGUAGE;
+
+  @observable
+  error: SearchError | null = null;
+}
+
+export const store = new Store();
+export const StoreContext = createContext(store);
