824020e650c72bc212c15a38b20c713cd0af5174
Xiaoru Li <dev@xiaoru.li>
2019-07-21 20:23:46 +0200

    Add App/Main/ResultsList

---
+++ src/components/App/Main/ResultsList.tsx
@@ -0,0 +1,48 @@
+import React, { useContext } from "react";
+import { Text, Box, Color } from "ink";
+import { observer } from "mobx-react-lite";
+
+import { Page } from "../../../api";
+import { StoreContext } from "../../../store";
+
+export const ResultsList = observer(() => {
+  const store = useContext(StoreContext);
+
+  return (
+    <Box flexDirection={"column"}>
+      {store.results.map((result, i) => {
+        return <ResultCard result={result} i={i} key={i} isSelected={store.selectedIndex === i} />;
+      })}
+    </Box>
+  );
+});
+
+const ResultCard = (props: { result: Page; i: number; isSelected: boolean }) => {
+  return (
+    <Box height={3}>
+      <Box width={2} marginX={1} flexDirection={"row-reverse"}>
+        {props.isSelected ? (
+          <Color green>
+            <Text bold>>> </Text>
+          </Color>
+        ) : (
+          <Color green>
+            <Text bold>{props.i + 1}. </Text>
+          </Color>
+        )}
+      </Box>
+      <Box flexDirection="column">
+        {props.isSelected ? (
+          <Color bgGreen white>
+            <Text bold>{props.result.title}</Text>
+          </Color>
+        ) : (
+          <Color green>
+            <Text bold>{props.result.title}</Text>
+          </Color>
+        )}
+        <Box textWrap={"wrap"}>{props.result.description}</Box>
+      </Box>
+    </Box>
+  );
+};
---
+++ src/components/App/Main/index.tsx
@@ -0,0 +1,38 @@
+import React, { useContext } from "react";
+import { Box } from "ink";
+import { Key } from "readline";
+import open from "open";
+import { ENTER, ARROW_DOWN, ARROW_UP } from "../../../keys";
+
+import { useKeypress } from "../../../hooks/useKeypress";
+import { StoreContext, Store } from "../../../store";
+import { ResultsList } from "../Main/ResultsList";
+import { SearchBar } from "../Main/SearchBar";
+
+export const Main = () => {
+  const store = useContext(StoreContext);
+  useKeypress((_?: string, key?: Key) => handleKeypress(store, _, key));
+
+  return (
+    <Box flexDirection={"column"}>
+      <SearchBar />
+      <ResultsList />
+    </Box>
+  );
+};
+
+function handleKeypress(store: Store, _?: string, key?: Key) {
+  if (!key) return;
+
+  switch (key.sequence) {
+    case ARROW_UP:
+      store.selectPrev();
+      break;
+    case ARROW_DOWN:
+      store.selectNext();
+      break;
+    case ENTER:
+      store.toggleDetails();
+      break;
+  }
+}
