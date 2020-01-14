415c789bbe6b98dde3653e584a9e97c8e59dadde
Xiaoru Li <dev@xiaoru.li>
2020-01-14 02:20:29 +0100

    Remove unused keys.ts

--- src/keys.ts
+++
@@ -1,7 +0,0 @@
-export const CTRL_C: string = "\u0003";
-export const ENTER: string = "\r";
-export const ARROW_UP: string = "\u001b[A";
-export const ARROW_DOWN: string = "\u001b[B";
-export const TAB: string = "\t";
-export const SHIFT_TAB: string = "\u001b[Z";
-export const ESC: string = "\u001b";
